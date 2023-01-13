d261c86e6b4d23bee139373aebd7a4206aae5014
jaintushaar <119755757+jaintushaar@users.noreply.github.com>
2023-01-13 16:54:07 +0530

    Update problem 1.cpp

--- problem 1.cpp
+++ problem 1.cpp
@@ -1,42 +1,48 @@
-//constructors in rectangle
-#include<iostream>
-using namespace std;
-class Rectangle
+#include <iostream>
+#include <string>
+#include <vector>
+
+class hello
 {
-private:
+public:
 int length;
 int breadth;
-public:
-Rectangle()
+hello()
 {
-length=0;
-breadth=0;
+int length = 0;
+int breadth = 0;
 }
-Rectangle(int x)
+hello(int lenght)
 {
-length=x;
-breadth=x;
+// this(5,8);
+this->length = 4;
+breadth = 5;
 }
-Rectangle(int x,int y)
+hello(int length, int breadth)
 {
-length=x;
-breadth=y;
+this->length = 7;
+this->breadth = 8;
 }
-int getarea()
+void m1()
 {
-return length*breadth;
-
+std::cout << length * breadth << std::endl;
 }
-
 };
-int main()
+class Recatangle8
 {
-Rectangle R1;
-Rectangle R2(10);
-Rectangle R3(2,3);
-
-cout<<R1.getarea()<<endl;
-cout<<R2.getarea()<<endl;
-cout<<R3.getarea()<<endl;
-
+public:
+static void main(std::vector<std::string> &args)
+{
+hello* obj = new hello();
+obj->m1();
+hello* obj1 = new hello(10);
+obj1->m1();
+hello* obj2 = new hello(12, 2);
+obj2->m1();
 }
+};
+int main(int argc, char **argv){
+std::vector<std::string> parameter(argv + 1, argv + argc);
+Recatangle8::main(parameter);
+return 0;
+};
